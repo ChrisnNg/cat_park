98c54edcbc10ceee6480f3515d5c8bdfd67d84bd
anthonyzhu132 <anthonyzhu132@gmail.com>
2019-10-28 12:49:17 -0700

    updating

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav.js";
