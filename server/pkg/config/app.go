22ff4dd3a1eeabc9094196363e20fa63e4045297
Thomas <ThomasBogdanov1@gmail.com>
2019-10-28 18:52:16 -0700

    Backend API operational

--- server/pkg/config/app.go
+++ server/pkg/config/app.go
@@ -7,43 +7,23 @@ import (
 	
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "password"
-	dbname   = "cat_park"
-)
-
 var (
 	db * gorm.DB
 )
 
-func Connect() {
-	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-	// 	"password=%s dbname=%s sslmode=disable",
-	// 	host, port, user, password, dbname)
-	// db, err := sql.Open("postgres", psqlInfo)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer db.Close()
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=cat_park password=123")
+
+
+func Connect() {
+	d, err := gorm.Open("postgres", "host=localhost port=5432 user=vagrant dbname=cat_park password=123")
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	db = d
 
 	fmt.Println("Successfully connected!")
 }
 
-func getDB() *gorm.DB {
+func GetDB() *gorm.DB {
 	return db
 }
-
--- server/pkg/models/models.go
+++ server/pkg/models/models.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"github.com/ChrisnNg/cat_park/client/pkg/config"
+	"github.com/ChrisnNg/cat_park/server/pkg/config"
 	"github.com/jinzhu/gorm"
 )
 
@@ -9,24 +9,24 @@ var db *gorm.DB
 
 type User struct {
 	gorm.Model
-	Name string `gorm:not null`
-	Username string `gorm:unique;not null`
-	Password string `gorm: not null`
+	Name string
+	Username string
+	Password string
 	Karma int 
 }
 
 type Parkings struct {
 	gorm.Model
-	longitude int `gorm:not null`
-	latitude int `gorm:not null`
+	longitude int
+	latitude int
 	rating int `gorm:"default:0"`
 	karma int `gorm:"default:0"`
-	photo_url 
+	photo_url string
 }
 
 func init() {
 	config.Connect()
-	db = config.GetDB()
+	db := config.GetDB()
 	db.AutoMigrate(&User{})
 	db.AutoMigrate(&Parkings{})
 }
