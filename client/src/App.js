eb54bc46bdd36504f5191aa813b9431471b32b1f
ChrisnNg <cristopherng@hotmail.com>
2019-10-30 13:42:32 -0700

    add bootstrap stylesheet

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,6 @@ import Nav from "./components/Nav.js";
 import MaterialUiForm from "./components/Form.js";
 import FooterPage from "./components/Footer.js";
 import AboutPage from "./components/About.js";
-<link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-  crossorigin="anonymous"
-/>;
 
 class App extends Component {
   render() {
