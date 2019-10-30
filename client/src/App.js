fdd4d1a98a6571a882ebdb268083d77eb3f09733
anthonyzhu132 <anthonyzhu132@gmail.com>
2019-10-29 19:35:03 -0700

    added map component with market to change accordingly from onCLick event

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav.js";
 import MaterialUiForm from "./components/Form.js";
 import FooterPage from "./components/Footer.js";
+import Mapping from "./components/Map.js";
 
 class App extends Component {
   render() {
@@ -12,6 +12,9 @@ class App extends Component {
         <Nav />
         <div id="add-location-form">
           <MaterialUiForm />
+        </div>
+        <div id="map">
+          <Mapping />
         </div>
         <div id="footer-id">
           <FooterPage />
