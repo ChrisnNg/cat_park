af33ced1718b89e1e6ac1587d9a6a1da24873f22
Thomas <ThomasBogdanov1@gmail.com>
2019-10-30 11:40:58 -0700

    Switch case refactoring

--- server/cmd/main/main.go
+++ server/cmd/main/main.go
@@ -19,30 +19,27 @@ func main() {
 	wordPtr := flag.String("task", "foo", "a string")
 	flag.Parse()
 
-	if *wordPtr == "ResetDB" {
+	switch *wordPtr {
+	case "ResetDB":
 		fmt.Println("Resetting Database . . .")
 		db.DropTableIfExists(models.Users{}, models.Parkings{}, models.Crimes{})
 		db.AutoMigrate(models.Users{}, models.Parkings{}, models.Crimes{})
 		fmt.Println("Reset Complete!")
-	}
-
-	if *wordPtr == "DropTables" {
+	case "DropTables":
 		fmt.Println("Dropping Tables . . .")
 		db.DropTableIfExists(models.Users{}, models.Parkings{}, models.Crimes{})
 		fmt.Println("Tables Dropped!")
-	}
-
-	if *wordPtr == "SeedTables" {
+	case "SeedTables":
 		fmt.Println("Seeding Tables . . .")
-
-	}
-
-	if *wordPtr == "Start" {
+	case "Start":
 		fmt.Println("Starting Server . . .")
 		r := mux.NewRouter()
 		routes.RegisterParkingSpotRoutes(r)
 		http.Handle("/", r)
 		fmt.Println("Server listening on port 8000!")
 		log.Fatal(http.ListenAndServe(":8000", r))
+	default:
+		fmt.Println("Please enter a valid flag")
 	}
+
 }
