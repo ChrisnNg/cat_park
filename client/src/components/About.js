d51f167d02f523e10016522115141b344e4fb211
ChrisnNg <cristopherng@hotmail.com>
2019-10-30 13:09:22 -0700

    add thomas social medias

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -26,52 +26,97 @@ const AboutPage = () => {
           <li> Hosted on Heroku & Netlify </li>
         </ul>
       </header>
-      <FlexView vAlignContent="center" hAlignContent="center">
-        I'm vertically centered!
-      </FlexView>
-      <div className="container text-center">
+      <FlexView hAlignContent="center">
         <p>Follow us on social media to get our latest updates!</p>
-        <div className="socialMediaIcons chris">
-          <a href="https://www.facebook.com/christopher.ng.5203?ref=bookmarks">
-            <img
-              src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/fb-icon.png?raw=true"
-              alt="Chris-fb"
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/christopherkyleng/">
-            <img
-              src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/linked-icon.png?raw=true"
-              alt="Chris-linkedin"
-            />
-          </a>
-          <a href="https://www.instagram.com/cristopherng/">
-            <img
-              src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/insta-icon.png?raw=true"
-              alt="Chris-ig"
-            />
-          </a>
-        </div>
-        <div className="socialMediaIcons anthony">
-          <a href="https://www.facebook.com/xoxobbq">
-            <img
-              src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/fb-icon.png?raw=true"
-              alt="Anthony-fb"
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/anthony-zhu-3abb9b183/">
-            <img
-              src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/linked-icon.png?raw=true"
-              alt="Anthony-linkedin"
-            />
-          </a>
-          <a href="https://www.instagram.com/zhudriven/">
-            <img
-              src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/insta-icon.png?raw=true"
-              alt="Anthony-ig"
-            />
-          </a>
-        </div>
-      </div>
+      </FlexView>
+      <FlexView className="leftName" hAlignContent="left">
+        <span>
+          <b>Christopher Ng</b>
+        </span>
+      </FlexView>
+      <FlexView className="centerName" hAlignContent="center">
+        <span>
+          <b>Anthony Zhu</b>
+        </span>
+      </FlexView>
+      <FlexView className="rightName" hAlignContent="right">
+        <span>
+          <b>Thomas Bogdanov</b>
+        </span>
+      </FlexView>
+      <FlexView
+        className="socialMediaIcons leftMedia"
+        vAlignContent="center"
+        hAlignContent="left"
+      >
+        <a href="https://www.facebook.com/christopher.ng.5203?ref=bookmarks">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/fb-icon.png?raw=true"
+            alt="Chris-fb"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/christopherkyleng/">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/linked-icon.png?raw=true"
+            alt="Chris-linkedin"
+          />
+        </a>
+        <a href="https://www.instagram.com/cristopherng/">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/insta-icon.png?raw=true"
+            alt="Chris-ig"
+          />
+        </a>
+      </FlexView>
+
+      <FlexView
+        className="socialMediaIcons centerMedia"
+        vAlignContent="center"
+        hAlignContent="center"
+      >
+        <a href="https://www.facebook.com/xoxobbq">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/fb-icon.png?raw=true"
+            alt="Anthony-fb"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/anthony-zhu-3abb9b183/">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/linked-icon.png?raw=true"
+            alt="Anthony-linkedin"
+          />
+        </a>
+        <a href="https://www.instagram.com/zhudriven/">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/insta-icon.png?raw=true"
+            alt="Anthony-ig"
+          />
+        </a>
+      </FlexView>
+      <FlexView
+        className="socialMediaIcons rightMedia"
+        vAlignContent="center"
+        hAlignContent="right"
+      >
+        <a href="">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/fb-icon.png?raw=true"
+            alt="Thomas-fb"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/anthony-zhu-3abb9b183/">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/linked-icon.png?raw=true"
+            alt="Thomas-linkedin"
+          />
+        </a>
+        <a href="https://www.instagram.com/zhudriven/">
+          <img
+            src="https://github.com/ChrisnNg/cat_park/blob/features/about_us/client/public/insta-icon.png?raw=true"
+            alt="Thomas-ig"
+          />
+        </a>
+      </FlexView>
     </section>
   );
 };
