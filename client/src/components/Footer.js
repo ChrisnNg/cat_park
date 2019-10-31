a5a842a14917e74240e7c78a65ecec522448ef8c
anthonyzhu132 <anthonyzhu132@gmail.com>
2019-10-30 18:56:06 -0700

    added footer information

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,8 +6,16 @@ const FooterPage = () => {
     <footer>
       <div className="footer-limiter">
         <div className="footer-left">
-          <p className="footer-links">Footer</p>
-          <p>CAT Park © 2019</p>
+          <p className="footer-links">CAT Park © 2019</p>
+          <p>
+            Designed to help you find the closest free parking area available.
+            Uses a dataset from the Vancouver Police Department's crime API so
+            you can gauge risk for where you park.
+          </p>
+          <br></br>
+          <h5>
+            <a href="#">About us</a>
+          </h5>
         </div>
       </div>
     </footer>
