d2b56f822c4b27fc8bc09258c1b2f52d46e55ae8
ChrisnNg <cristopherng@hotmail.com>
2019-10-31 19:12:44 -0700

    add login button and style it

---
+++ client/src/components/Login.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./MyAccountEdit.css";
+
+const thumpsUp = "client/public/thumbs-up.png";
+
+const Login = props => {
+  return (
+    <section>
+      <form className="editAccount">
+        <div className="form-group row">
+          <label htmlFor="Username" className="col-sm-2 col-form-label">
+            Username
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control"
+              id="Username"
+              placeholder="Username"
+            />
+          </div>
+          <label htmlFor="Password" className="col-sm-2 col-form-label">
+            Password
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="password"
+              className="form-control"
+              id="Password"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
