3ea8f63ada025543c371ea26584b4cd995405f51
ChrisnNg <cristopherng@hotmail.com>
2019-11-03 19:34:11 -0800

    park icon created and appended to example marker

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -8,6 +8,7 @@ import {
 
 import CurrentLocation from "./CurrentLocation";
 import three_cats from "../../../public/three_cats_icon.png";
+import cat_park_icon from "../../../public/cat_park_icon.png";
 
 const gradient = [
   "rgba(0, 255, 255, 0)",
@@ -69,6 +70,9 @@ export class MapContainer extends React.Component {
             name={"Test Marker"}
             position={{ lat: 49.280385, lng: -123.096307 }}
             onClick={this.onMarkerClick}
+            icon={{
+              url: cat_park_icon
+            }}
           />
 
           <InfoWindow
